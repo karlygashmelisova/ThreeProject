bbe9b1d17f15650c5e61c4f3efb3a908d2e5f778
Melisova Karlygash <melisovakarlygash@gmail.com>
2017-11-09 11:39:31 +0600

    commit

--- src/kz/kazntu/ParameterTest1Servlet.java
+++ src/kz/kazntu/ParameterTest1Servlet.java
@@ -40,7 +40,7 @@ public class ParameterTest1Servlet extends HttpServlet {
         resp.setContentType("text/html");
 
         PrintWriter out = resp.getWriter();
-        String title = "Using POST Method to Read Form Data";
+        String title = "Using POST Method to Read Form Dataaaa";
         String docType =
                 "<!doctype html>\n";
 
